c22093ad5f8dc32f75bbcc6acb35606e2f10e749
hsyang011 <hsyang011@gmail.com>
2023-07-31 17:48:13 +0900

    2023-07-31 JDBC 수업

--- src/ex00study/Study.java
+++ src/ex00study/Study.java
@@ -3,24 +3,14 @@ package ex00study;
 public class Study {
 
 	public static void main(String[] args) {
-		// 배열의 합 출력
-		int init = 0;
-		int num = sumArray(init);
-		System.out.printf("배열의 총합은 %d입니다.\n", num);
-	}
-	
-	static int sumArray(int init) {
-		// 변수와 배열 선언
-		int sum = init;
-		int[] arr = { 1,2,3,4,5,6,7,8,9,10 };
+		String str = "Hello   hi  a bc";
 		
-		// 배열의 총합
-		for (int n : arr) {
-			sum += n;
+		String[] arr = str.split(" ");
+		
+		int idx = 0;
+		for (String s : arr) {
+			System.out.printf("arr[%d]:%s ", idx++, s);
 		}
-		
-		// sum 반환
-		return sum;
 	}
 
 }
